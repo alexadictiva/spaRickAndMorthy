42723b2986922cdfae7f8bd83e27087a6c170b3a
Libia Perez <alexandra220790@gmail.com>
2021-07-07 20:58:32 -0300

    Obteniendo  los personajes con la funcion que llama a la API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,20 @@
-const Home = () => {
+import getData from "./../utils/getData";
+const Home = async () => {
+  const characters = await getData();
   const view = `
-  <div  class="Characters">
+  <div class="Characters">
+    ${characters.results
+      .map(
+        (character) => `
     <article class="Character-items">
-      <a href="#/1/">
-        <img src="image" alt="name"/>
-        <h2>Nombre</h2>
+      <a href="#/${character.id}/">
+        <img src="${character.image}" alt="${character.name}"/>
+        <h2>${character.name}</h2>
       </a>
     </article>
+    `
+      )
+      .join("")}
   </div>
   `;
   return view;
