bad3a6916239ccbe0cf63670eb39ce4cfcd2be38
Libia Perez <alexandra220790@gmail.com>
2021-07-07 19:11:45 -0300

    Creando el template del Home

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+const Home = () => {
+  const view = `
+  <div  class="Characters">
+    <article class="Character-items">
+      <a href="#/1/">
+        <img src="image" alt="name"/>
+        <h2>Nombre</h2>
+      </a>
+    </article>
+  </div>
+  `;
+  return view;
+};
+export default Home;
