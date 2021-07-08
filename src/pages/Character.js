b1f8e71a89d2d743b053a27e3cbc956625bd884a
Libia Perez <alexandra220790@gmail.com>
2021-07-07 21:03:33 -0300

    Conectando la funcion coon la descripcion de los personajes

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,17 +1,22 @@
-const Character = () => {
+import getData from "../utils/getData";
+import getHash from "../utils/getHash";
+
+const Character = async () => {
+  const id = getData();
+  const character = await getData(id);
   const view = `
   <div class="Characters-inner">
     <article class="Characters-card">
-      <img src="imagen" alt="nombre del personaje">
-      <h2>Nombre del personaje</h2>
+      <img src="${character.image}" alt="${character.name}">
+      <h2>${character.name}</h2>
     </article>
     <article class="Characters-card">
-      <h3>Episode: <span></span></h3>
-      <h3>Status: <span></span></h3>
-      <h3>Species: <span></span></h3>
-      <h3>Gender: <span></span></h3>
-      <h3>Origin: <span></span></h3>
-      <h3>Last Location: <span></span></h3>
+      <h3>Episode: <span> ${character.episode.lenght} </span></h3>
+      <h3>Status: <span>${character.status}</span></h3>
+      <h3>Species: <span>${character.species}</span></h3>
+      <h3>Gender: <span>${character.gender}</span></h3>
+      <h3>Origin: <span>${character.origin.name}</span></h3>
+      <h3>Last Location: <span>${character.location.name}</span></h3>
     </article>
   </div>  
   `;
