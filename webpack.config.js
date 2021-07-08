b938f906fb1b1ed7d18b194b98d217ce8d2276d2
Libia Perez <alexandra220790@gmail.com>
2021-07-07 21:18:14 -0300

    Agregando el plugin al webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -27,5 +28,11 @@ module.exports = {
       template: "./public/index.html",
       filename: "./index.html",
     }),
+    new CopyWebpackPlugin([
+      {
+        from: "./src/styles/styles.css",
+        to: " ",
+      },
+    ]),
   ],
 };
